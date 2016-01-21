a46ea9f5ff43aeba691f5b7b071c45588889f212
Dale Anderson <danderson00@gmail.com>
2016-01-20 18:04:43 -0800

    Added stuff

--- combined/server.js
+++ combined/server.js
@@ -1,8 +1,16 @@
 var express = require('express'),
+    https = require('https'),
+    fs = require('fs'),
     app = express(),
     mobileApp = require('azure-mobile-apps')({ skipVersionCheck: true })
 
 mobileApp.tables.add('todoitem')
 app.use(mobileApp)
 app.use(express.static(__dirname + '/static'))
-app.listen(process.env.PORT || 3000)
+//app.listen(process.env.PORT || 3000)
+
+var https_options = {
+    key: fs.readFileSync('./key.pem'),
+    cert: fs.readFileSync('./cert.pem')
+};
+https.createServer(https_options, app).listen(3000);
